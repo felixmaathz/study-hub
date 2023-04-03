048273495e4c9ea65439597e3defb3d4c2677167
SimonOlofsson <104519268+SimonOlofsson@users.noreply.github.com>
2023-04-03 14:32:16 +0200

    Hugo Scaling med simons knappar och bajs

--- components/LoggedInPage/HelpPopup.js
+++ components/LoggedInPage/HelpPopup.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import styles from '../../styles/popup.module.css'
-import logoType from "public/images/logotype.png";
-import Image from "next/image";
 
 
 function HelpPopup(props){
