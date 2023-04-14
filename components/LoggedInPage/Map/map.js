538eb59492053afb2d9372aa01fb9797880d8ccf
MartensGabriel <gabriel.martens.9695@student.uu.se>
2023-04-14 10:41:16 +0200

    Profilepopup in map.js gets user info from db

--- components/LoggedInPage/Map/map.js
+++ components/LoggedInPage/Map/map.js
@@ -5,6 +5,10 @@ import React, { useState } from "react";
 import ProfilePopup from "../ProfilePopup";
 import styles from '../../../styles/clearButton.module.css';
 
+import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {app, db} from "../../../config/firebaseConfig";
+import {collection, getDoc, doc, getDocs} from "firebase/firestore";
+
 
 //With inspiration from  "https://codesandbox.io/s/how-to-save-marker-location-when-adding-a-marker-with-onclick-on-map-in-react-leaflet-v3x-lghwn?file=/src/MyMap.jsx:0-41"
 
@@ -12,31 +16,67 @@ let yourActiveMarker = null;
 
 export default function Map()  {
 
-
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [major, setMajor] = useState("");
     const [profilePopup, setProfilePopup] = useState(false);
 
-    const yourIcon = L.icon({
-        iconSize: [30, 30],
-        iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+    const auth = getAuth(app);
+    const user = auth.currentUser;
+    console.log(user)
+    let uid = ""
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            uid = user.uid;
+            console.log("User is logged in " + uid)
+            getUserData().then(r => {
+                console.log("username: " + username+ " email: " + email + " major: " + major)
+            })
+
+        } else {
+            console.log("User is not logged in")
+        }
     });
 
-    const theirIcon = L.icon({
-        iconSize: [30, 30],
-        iconUrl: "https://cdn-icons-png.flaticon.com/512/9131/9131546.png",
-    });
+    const getUserData = async () => {
+        console.log(uid)
+        const docRef = doc(db, "users", uid);
+        try {
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setUsername(docSnap.data().username)
+                setEmail(docSnap.data().email)
+                setMajor(docSnap.data().major)
 
+            } else {
+                console.log("Document does not exist")
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
     function Markers()  {
+        const yourIcon = L.icon({
+            iconSize: [30, 30],
+            iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
+        });
 
+        const theirIcon = L.icon({
+            iconSize: [30, 30],
+            iconUrl: "https://cdn-icons-png.flaticon.com/512/9131/9131546.png",
+        });
         const map = useMapEvents({
             click: (e) => {
 
                 if (yourActiveMarker) {
                     yourActiveMarker.remove();
                 }
-                const { lat, lng } = e.latlng;
 
+                const { lat, lng } = e.latlng;
                 //Your own marker
                 yourActiveMarker = L.marker([lat, lng], { icon: yourIcon }).addTo(map).on("click", () => {
                     triggerPopup();
@@ -45,7 +85,7 @@ export default function Map()  {
         });
 
         //Other peoples markers, for now hardcoded markers
-        const fogenIndustries = [
+        /*const fogenIndustries = [
             ["Glocken", 59.852320, 17.611310],
             ["Fogen", 59.859370, 17.611960],
             ["Simpy", 59.847410, 17.583440],
@@ -57,7 +97,8 @@ export default function Map()  {
                 triggerPopup()
                 alert(fogenIndustries[i][0])
             });
-        }
+        }*/
+
         const removeMyMarker = () => {
             if (yourActiveMarker) {
                 yourActiveMarker.remove();
@@ -88,6 +129,11 @@ export default function Map()  {
                 <Markers/>
             </MapContainer>
             {/*<ProfilePopup trigger={profilePopup} setTrigger={setProfilePopup} />*/}
+            <ProfilePopup trigger={profilePopup} setTrigger={setProfilePopup} data={{
+                username: username,
+                email: email,
+                major: major
+            }}/>
         </div>
     );
 }
