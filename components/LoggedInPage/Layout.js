2bf0186307024efe25784294f0856abdba4aad21
felixmaathz <94930466+felixmaathz@users.noreply.github.com>
2023-04-19 13:42:10 +0200

    editprofile protoyp

--- components/LoggedInPage/Layout.js
+++ components/LoggedInPage/Layout.js
@@ -8,6 +8,7 @@ export default function Layout({ children, home}){
         <div>
             <Head>
                 <title>StudyHub</title>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
             </Head>
             <Navbar/>
             <div>
