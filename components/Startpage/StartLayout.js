0a25ac3dbebd4533f82e6cf3af7b455d78d370f5
felixmaathz <94930466+felixmaathz@users.noreply.github.com>
2023-04-25 13:20:04 +0200

    signup validation och bug fix

--- components/Startpage/StartLayout.js
+++ components/Startpage/StartLayout.js
@@ -8,6 +8,7 @@ export default function StartLayout({ children, home}){
             <Head>
                 <title>StudyHub</title>
                 <link rel="icon" href="/images/favicon.png" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
             </Head>
 
             <StartHeader/>
