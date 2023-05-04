8a4472315d13e17f959721b032b3dd884c227835
felixmaathz <94930466+felixmaathz@users.noreply.github.com>
2023-05-04 13:06:06 +0200

    minimal style på notiser

--- components/LoggedInPage/NotificationPopup.js
+++ components/LoggedInPage/NotificationPopup.js
@@ -18,11 +18,13 @@ export default function NotificationPopup(props) {
                     try {
                         await getDoc(doc(db, "users", like.userLikeID)).then(
                             (doc) => {
-                                setUsernames((usernames) => [...usernames, {username: doc.data().username, time: like.timestamp.toDate()}])
+                                setUsernames((usernames) => [...usernames, {
+                                    username: doc.data().username,
+                                    time: like.timestamp.toDate()
+                                }])
                             }
                         )
-                    }
-                    catch (e) {
+                    } catch (e) {
                         console.log(e)
                     }
                 })
@@ -38,27 +40,40 @@ export default function NotificationPopup(props) {
 
 
     return (props.notificationTrigger) ? (
-        <div className={styles.editPopup}>
+        <>
             <div style={{width: "100vw", height: "100vh", position: "absolute"}}
                  onClick={closePopup}></div>
             <div className={styles.notificationPopupInner}>
-
-                {usernames.map((user) => (
-
-                    <div className={styles.notificationScroll}>
+                <div onClick={() => props.setNotificationTrigger(false)} className={styles.closeBtn}>
+                        <span
+                            className="material-symbols-outlined">
+                            close
+                            </span>
+                </div>
+                <div className={styles.notificationScroll}>
+                    {usernames.map((user) => (
                         <div className={styles.notificationText}>
                             <p>{user.username} has liked your profile!</p>
-                            <p>{dayList[user.time.getDay() - 1].substring(0,3)} {user.time.toLocaleTimeString('en-US', {
-                                hour: '2-digit',
-                                minute: '2-digit',
-                            })}</p>
+                            {((Date.now() - user.time.getTime()) < 604800000) ? (
+                                <p>{dayList[user.time.getDay() - 1].substring(0, 3)}{user.time.toLocaleTimeString('en-US', {
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                })}</p>
+                            ) : (
+                                <p>{user.time.toLocaleDateString() + " "}{user.time.toLocaleTimeString('en-US', {
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                })}</p>
+                            )
+                            }
+
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
 
 
             </div>
-        </div>
+        </>
     ) : "";
 
 }
